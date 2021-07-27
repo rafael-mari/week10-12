28a7ae633f7733207f9c7f1a6dd6bbc506980ba9
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-07-27 18:52:11 -0500

    Update chapter14.ex01count.c

--- chapter14.ex01count.c
+++ chapter14.ex01count.c
@@ -4,10 +4,10 @@
 const char file1[] = "input.txt"; //the file I create where I write some stuff
 const char file2[] = "output.txt";//the file that the program will create where the number of characters will be printed
 
-int main()
+int main(void)
 {
-	int num = 0;//the integer number starts with 0
-	FILE *in_file; //I enter the file
+	int lines = 0; //the integer lines starts with 0
+	FILE *in_file; //ingreso del archivo
 
 	int character; //To check each one of the characters in the file
 
@@ -17,18 +17,18 @@ int main()
 		printf("The file '%s' was not found\n", file1);
 		exit(8); //If this happens it will finish the program
 	}
-	while (1) {
-		character = fgetc(in_file); 
-		if (character == EOF)  //If while scanning the file we get to the EOF (end of file), break the loop and finish the count.
-			break;
-		++num; //But if there isn't the EOF yet, it will add 1 to the count in the va
+
+	lines = 0; //the integer lines starts with 0
+	for (character = getc(in_file); character != EOF; character = getc(in_file)) { //while the current character is not the end of file, do the following:
+		if (character == '\n') //if the character is a backslash n (a line break), do the following
+			++lines; //add 1 to the count of lines
 	}
 
-	printf("Number of characters in %s is %d\n", file1, num); //I print the number of characters of the input file, which is stored in the variable num
+	printf("The number of lines in %s is %d\n", file1, lines+1); //I print the number of lines of the input file, which is stored in the variable lines, I add 1 to the variable lines because even when you don't end a text with a line break, it is still a line
 	fclose(in_file); //Close the first file
   FILE *out_file;
   out_file = fopen(file2, "wr"); //Open a new file where I will print the same thing as before
-  fprintf(out_file,"Number of characters in %s is %d\n", file1, num); //I print the number of characters of the input file, which is stored in the variable num but now in the new file named output.txt
+  fprintf(out_file,"The number of lines in %s is %d\n", file1, lines+1); //I print the number of lines of the input file, which is stored in the variable lines but now in the new file named output.txt
   fclose(out_file); //Close the second file
 	return 0;
 }
